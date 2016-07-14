9cc4a2ec2ad14836de9b6cf89d1620c4a3d53eff
Alyssia Matthews <vwcruiser61@hotmail.com>
2016-07-14 17:51:09 +0000

    added model to grass.png

--- staic/app.js
+++ staic/app.js
@@ -15,3 +15,25 @@ var message = document.getElementById("message");
 document.getElementById("myButton").addEventListener("click", function() {
     alert( message.value );
 });
+
+// Get th modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it insie modal while using the alt text as caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+};
+
+// gets the <span> element that closes the modal
+var span = document.getElementsByClassName("Close")[0];
+
+// When user clicks on <span> (x) it close the modal
+span.click = function() {
+    modal.style.display = "none";
+};
